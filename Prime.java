82246269b49602f89ddf25eb95e916d5432a5cec
Charles Martinez <Chuckmode@charless-air.attlocal.net>
2016-05-08 17:14:35 -0500

    prime.java

--- Prime.java
+++ Prime.java
@@ -1,4 +1,31 @@
+import java.util.*;
+
 public class Prime
 {
-    public static int a() { return 1; }
+    public static void main (String[] args)
+    {
+        int i =0;
+        int num =0;
+
+        ArrayList<List<Object>> primes = new ArrayList<List<Object>>();
+
+        for (i = 1; i <= 100; i++)
+        {
+            int counter=0;
+            for(num =i; num>=1; num--)
+            {
+                if(i%num==0)
+                {
+                    counter = counter + 1;
+                }
+            }
+            if (counter ==2)
+            {
+                primes = primes.append(i);
+            }
+        }
+        System.out.println("Prime numbers from 1 to 100 are :");
+        System.out.println(primes);
+    }
+    public static int a() { return primes; }
 }
